44d054b01ff754275f78fba4970d861f07d4504f
Mordenius Norrogem <mordenius@gmail.com>
2018-08-18 01:05:36 +0300

    chore(index.d.ts) declare Request

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,13 @@
+import http from "http";
+
 declare class FS {
 	public static openFile(filePath: string): Promise<number | never>;
 	public static closeFile(fd: number): Promise<{} | never>;
 	public static createDir(fullpath: string): Promise<void | never>
+}
+
+declare class Request {
+	public static get(_OPTIONS: http.RequestOptions, checkLength?: boolean): Promise<string | never> 
+	public static getJson(_OPTIONS: http.RequestOptions, checkLength?: boolean): Promise<object | never> 
+	public static getResponse(_OPTIONS: http.RequestOptions): Promise<http.IncomingMessage | never> 
 }
